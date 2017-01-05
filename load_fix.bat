68525a00dd2e8887e479de0d15bf452b7512b53f
Gabor Biro <gabor.biro@yahoo.com>
2017-01-05 18:31:23 +0000

    Improved fixture loading

--- load_fix.bat
+++ load_fix.bat
@@ -1,6 +1,6 @@
 REM Windows only!
 
 REM populate database
-python ..\manage.py loaddata fixtures\category_db.json
-python ..\manage.py loaddata fixtures\company_db.json
-python ..\manage.py loaddata fixtures\subscriptions_db.json
+python manage.py loaddata subscriptions_lib\fixtures\category_db.json
+python manage.py loaddata subscriptions_lib\fixtures\company_db.json
+python manage.py loaddata subscriptions_lib\fixtures\subscriptions_db.json
